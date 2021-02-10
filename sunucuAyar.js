4c3020d68f9eebdcf5ce5a71b75a5f7d66400ea8
SehinsahisGods <78857284+SehinsahisGods@users.noreply.github.com>
2021-02-10 17:09:53 +0300

    Add files via upload

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: "807869072871915560", // sunucu idsi
+  ownerRole:"807869073018585119", // sunucu sahiplerinin rolü
+  teyitsizRolleri:[""], // kayıt olmayanlara verieln rol
+  jailRolu:"", // cezalılara verilen rol
+  enAltYetkiliRolu:[''], // ilk yetkili permi
+  activity:"Ψ Developed by Şehinşah", // botun durumu
+  status:"online",
+  invitelink:"https://discord.gg/bzu9n2dYDH", //davet linki
+  symbol:"Ψ", // tag
+  voicechannel:"807891624792555530" // botun gireceği ses kanalı
+};
+module.exports = ayarlar;
